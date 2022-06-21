472c3f18ddeabffe3e72560b4c3faeeb67035014
Ameya <ameya@Ameyas-MacBook-Air.local>
2022-06-21 19:38:00 +0530

    Mongodb added in docker-compose

--- utils/mongodb.go
+++ utils/mongodb.go
@@ -2,7 +2,7 @@ package utils
 
 import (
 	"context"
-	"log"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -15,7 +15,8 @@ import (
 ******************************************/
 
 var client *mongo.Client
-
+var Version="1.0"
+var log = New().With(nil, "version", Version)
 /********
 *	connect To MongoDb and get Collection by passing documentName,tableName as a String
 * 	and Return the collection pointer
@@ -23,7 +24,7 @@ var client *mongo.Client
 func ConnectAndGetMongoDbCollection(documentName string, tableName string) *mongo.Collection {
 	// get a MongoDBClient
 	client := getClient()
-	log.Println("Connected to MongoDB!")
+	log.Info("Connected to MongoDb")
 	// Check the connection
 	collection := client.Database(documentName).Collection(tableName)
 	return collection
@@ -37,7 +38,7 @@ func ConnectAndGetMongoDbCollection(documentName string, tableName string) *mong
 func CloseMongoDbClient() {
 	client := getClient()
 	client.Disconnect(context.TODO())
-	log.Println("Connection to MongoDB closed.")
+	log.Info("Connected to MongoDb")
 }
 
 
@@ -45,14 +46,15 @@ func CloseMongoDbClient() {
 *	get MongoDb client
 *********/
 func getClient() *mongo.Client {
-	clientOptions := options.Client().ApplyURI("mongodb+srv://ameyasalagre:pass12345@cluster0-prqsb.mongodb.net/")
+	url:= GetEnv("MONGO_URL","localhost:12354")
+	clientOptions := options.Client().ApplyURI(url)
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
-		log.Fatal(err)
+		log.Error(err)
 		}
 	err = client.Ping(context.TODO(), nil)
 	if err != nil {
-		log.Fatal("Error While Connection", err)
+		log.Error("Error While Connection", err)
 	}
 	return client 
 }
